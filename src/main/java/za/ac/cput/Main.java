5780ec8de5a559cb0ba8396c0007b74417b195d4
Bekithemba Mrwetyana <222706066@mycput.ac.za>
2025-09-03 11:06:25 +0200

    Updated code

--- src/main/java/za/ac/cput/Main.java
+++ src/main/java/za/ac/cput/Main.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @SpringBootApplication
 public class  Main {
     public static void main(String[] args) {
